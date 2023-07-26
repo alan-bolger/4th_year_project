d84520e3f620d19ababde82d96c1cda4519e4e35
alan-bolger <alan.j.bolger@gmail.com>
2023-07-27 00:35:55 +0100

    Added utility functions.

--- ST_and_MT_Computations_in_Game Dev/h/Raytracer.h
+++ ST_and_MT_Computations_in_Game Dev/h/Raytracer.h
@@ -18,6 +18,8 @@
 
 #include <SFML/Graphics.hpp>
 
+constexpr auto PI = 3.141592653589793;
+
 using Vec3f = Vec3<float>;
 
 struct Sphere
@@ -31,10 +33,27 @@ struct Sphere
     Vec3f emissionColour;
     Vec3f rayOrigin;
 
+    /// <summary>
+    /// Sphere constructor.
+    /// </summary>
+    /// <param name="center">The sphere's center point.</param>
+    /// <param name="radius">The sphere's radius.</param>
+    /// <param name="surfaceColour">The sphere's surface colour.</param>
+    /// <param name="reflection">The sphere's reflection value.</param>
+    /// <param name="transparency">The sphere's transparency value.</param>
+    /// <param name="emission">The sphere's emission colour.</param>
     Sphere(const Vec3f &center, const float &radius, const Vec3f &surfaceColour, const float &reflection = 0.0f, const float &transparency = 0.0f, const Vec3f &emissionColour = 0.0f)
         :
         center(center), radius(radius), radius2(radius *radius), surfaceColour(surfaceColour), reflection(reflection), transparency(transparency), emissionColour(emissionColour) { }
 
+    /// <summary>
+    /// Compute a ray to sphere intersection using the geometric solution.
+    /// </summary>
+    /// <param name="rayOrigin">The origin point of the ray.</param>
+    /// <param name="rayDir">The direction of the ray.</param>
+    /// <param name="t0">Near value.</param>
+    /// <param name="t1">Far value.</param>
+    /// <returns>True if there's an intersection, false if there's no intersection.</returns>
     bool intersect(const Vec3f &rayOrigin, const Vec3f &rayDir, float &t0, float &t1) const
     {
         Vec3f l = center - rayOrigin;
@@ -60,11 +79,20 @@ public:
 	Raytracer(int w, int h);
 	~Raytracer();
     void handleUI();
+    void render();
 
 private:	
 	int renderW;
 	int renderH;
+    Vec3f rayOrigin;
+    int fov;
+    std::unique_ptr<ThreadPool> threadPool;
     std::unique_ptr<sf::RenderTexture> renderTexture;
+    std::vector<uint8_t> pixelArray;
+
+    float mix(const float &a, const float &b, const float &mix);
+    Vec3f trace(const Vec3f &rayOrigin, const Vec3f &rayDir, const int &depth);
+    void renderSection(sf::Vector2f pixTL, sf::Vector2f pixBR);
 };
 
 #endif // !RAYTRACER_H
--- ST_and_MT_Computations_in_Game Dev/src/Raytracer.cpp
+++ ST_and_MT_Computations_in_Game Dev/src/Raytracer.cpp
@@ -62,3 +62,94 @@ void Raytracer::handleUI()
 
 	ImGui::End();
 }
+
+/// <summary>
+/// This function divides the screen into sections.
+/// Each section can then be processed individually on its own thread.
+/// </summary>
+void Raytracer::render()
+{
+	// This renders using multi-threading
+	int numOfSections = 16;
+	int sectionWidth = renderW / numOfSections;
+
+	// Add the render jobs to the thread pool
+	for (int i = 0; i < numOfSections; ++i)
+	{
+		threadPool->addJob([=]
+			{
+				int startX = i * sectionWidth;
+				int endX = startX + sectionWidth;
+				renderSection(sf::Vector2f(startX, 0), sf::Vector2f(endX, renderH));
+			});
+	}
+
+	// This renders using a single thread
+	// renderSection({ 0, 0 }, { 1280, 720 });
+}
+
+/// <summary>
+/// Mix utility function.
+/// </summary>
+/// <param name="a">First value to mix.</param>
+/// <param name="b">Second value to mix.</param>
+/// <param name="mix">Mix value.</param>
+/// <returns>The resulting mixed value.</returns>
+float Raytracer::mix(const float &a, const float &b, const float &mix)
+{
+	return b * mix + a * (1 - mix);
+}
+
+/// <summary>
+/// This is the main trace function. It takes a ray as argument (defined by its origin
+/// and direction). We test if this ray intersects any of the geometry in the scene.
+/// If the ray intersects an object, we compute the intersection point, the normal
+/// at the intersection point, and shade this point using this information.
+/// Shading depends on the surface property (is it transparent, reflective or diffuse?).
+/// The function returns a colour for the ray. If no intersection occurs, the background
+/// colour is returned.
+/// </summary>
+/// <param name="rayOrigin">The origin point of the ray.</param>
+/// <param name="rayDir">The direction of the ray.</param>
+/// <param name="depth">Depth of recursion.</param>
+/// <returns>The ray colour.</returns>
+Vec3f Raytracer::trace(const Vec3f &rayOrigin, const Vec3f &rayDir, const int &depth)
+{
+	return Vec3f();
+}
+
+/// <summary>
+/// Raytrace a section of the image.
+/// We compute a camera ray for each pixel of the image,
+/// trace it and return a colour. If the ray hits a sphere, we return the colour of the
+/// sphere at the intersection point, otherwise we return the background colour.
+/// </summary>
+/// <param name="pixTL">The coordinate of the upper-left corner of the section.</param>
+/// <param name="pixBR">The coordinate of the lower-right corner of the section.</param>
+void Raytracer::renderSection(sf::Vector2f pixTL, sf::Vector2f pixBR)
+{
+	float invWidth = 1.0f / static_cast<float>(renderW);
+	float invHeight = 1.0f / static_cast<float>(renderH);
+	float aspectRatio = static_cast<float>(renderW) / static_cast<float>(renderH);
+	float angle = std::tan(PI * 0.5f * fov / 180.0f);
+
+	for (unsigned int y = pixTL.y; y < pixBR.y; ++y)
+	{
+		for (unsigned int x = pixTL.x; x < pixBR.x; ++x)
+		{
+			int index = (y * renderW + x);
+
+			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectRatio;
+			float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
+
+			Vec3f rayDir(xx, yy, -1);
+			rayDir.normalize();
+			Vec3f pixel = trace(rayOrigin, rayDir, 0);
+
+			pixelArray[index * 4] = (uint8_t)(std::min(1.0f, pixel.x) * 255);
+			pixelArray[(index * 4) + 1] = (uint8_t)(std::min(1.0f, pixel.y) * 255);
+			pixelArray[(index * 4) + 2] = (uint8_t)(std::min(1.0f, pixel.z) * 255);
+			pixelArray[(index * 4) + 3] = 255;
+		}
+	}
+}
